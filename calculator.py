1e106dcfa8a6510627f951bb4d6f9807995990f9
Hemalatha-2001 <152588399+Hemalatha-2001@users.noreply.github.com>
2023-12-08 06:52:40 -0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -5,32 +5,35 @@ def subtraction(a,b):
 def multiplication(a,b):   
    return a * b 
 def division(a,b):   
-   return a / b    
-print (" Select any Operation.")    
-print ("1. Addition")    
-print ("2. Subtraction")    
-print ("3. Multiplication")    
-print ("4. Division")    
+   return a / b
+
+while True:
+    print("Select any Operation.")    
+    print("1. Addition")    
+    print("2. Subtraction")    
+    print("3. Multiplication")    
+    print("4. Division")    
+    print("5. Exit")  
     
-choice = input(" Enter Choice (1/2/3/4): ")    
+    choice = input("Enter Choice (1/2/3/4/5): ")
     
-n1= int (input ("Enter the 1st number: "))    
-n2 = int (input ("Enter the 2nd number: "))    
+    if choice == '5':
+        print("Exiting the program. Goodbye!")
+        break  
     
-if choice == '1':    
-   print (n1, " + ", n2, " = ", addition(n1,n2))    
+    n1 = int(input("Enter the 1st number: "))
+    n2 = int(input("Enter the 2nd number: "))
     
-elif choice == '2':    
-   print (n1, " - ", n2, " = ", subtraction(n1,n2))    
-    
-elif choice == '3':    
-   print (n1, " * ", n2, " = ", multiplication(n1,n2))
-   
-elif choice == '4':
-    if n2 == 0:
-        print("Zero division error...Enter valid number")
+    if choice == '1':    
+        print(n1, " + ", n2, " = ", addition(n1, n2))    
+    elif choice == '2':    
+        print(n1, " - ", n2, " = ", subtraction(n1, n2))    
+    elif choice == '3':    
+        print(n1, " * ", n2, " = ", multiplication(n1, n2))
+    elif choice == '4':
+        if n2 == 0:
+            print("Zero division error... Enter a valid number")
+        else:    
+            print(n1, " / ", n2, " = ", division(n1, n2))
     else:    
-        print (n1, " / ", n2, " = ", division(n1,n2))
-   
-else:    
-   print ("Enter valid input")    
+        print("Enter valid input")
